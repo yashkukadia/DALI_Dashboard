c3c9345540e71c842104dbbff00cf2e3814bf202
Yash Kukadia <yashkukadia@yahoo.co.in>
2019-02-13 20:39:19 -0500

    First Commit for DALI Dashboard

---
+++ src/app/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get('https://raw.githubusercontent.com/dali-lab/mappy/gh-pages/members.json')
+  }
+
+}
